35de85931dde5681db4507f6edec453e56af6ed1
Roman Gonzalez <open-source@roman-gonzalez.info>
2023-04-21 23:12:17 -0700

    docs: update example to include more options

--- example/myproj/flake.nix
+++ example/myproj/flake.nix
@@ -19,15 +19,20 @@
       # managed by nixDir
       inputs = inputs;
       # specify the systems that are supported
-      systems = ["x86_64-linux" "aarch64-darwin"];
+      systems = ["x86_64-linux" "x86_64-darwin" "aarch64-darwin"];
       # specify the directory that contains the nix directory.
       root = ./.;
       # specify the directory with our nix config is called nix. This is the
       # default value, the declaration is here for demonstration purposes.
       dirName = "nix";
+      # inject the pre-commit of this project to all devShells and devenv configurations
+      injectPreCommit = true;
       # use a list of module names (e.g. nix/modules/devenv/my-hello-service) or
       # a boolean value to signal we want all our devenv modules imported when
       # we have an entry in the nix/devenvs directory.
       injectDevenvModules = [ "my-hello-service" ];
+      # use a list of overlay names (defined in nix/overlays.nix) that we want injected
+      # to the packages we import across all files in this project.
+      injectOverlays = [ "default" ];
     };
 }
